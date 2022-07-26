c4aae298a78d91a8cc8c68613e53978b155fad9b
trinh-hung137 <trinhconghung2001@gmail.com>
2022-07-26 23:59:56 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+     public function testFunction()
+    {
+        return 1;
+    }
+    public function tesAbc()
+    {
+        $data_user  = User::all();
+        return view('test',[
+            'data' => $data_user
+        ]);
+    }
+}
